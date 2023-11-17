8d985e020f40429b1d108519aa2c3b26f1ca8f2e
shivanshu.s-at-942428920331 <shivanshu.sanjeev@clappia.com>
2023-11-17 17:57:17 +0530

    Initial commit

---
+++ test/cenomi-integration.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CenomiIntegration = require('../lib/cenomi-integration-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cenomi-integration-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CenomiIntegration.CenomiIntegrationStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
