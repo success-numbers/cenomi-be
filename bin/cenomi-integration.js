b0a64361c3c9b55a7f57b66dce76b3752391bd53
Balaji Krishnan <balaji@snumbers.com>
2023-12-07 18:52:11 +0530

    Major Structural changes and fixes naming convention

--- bin/cenomi-integration.js
+++ bin/cenomi-integration.js
@@ -2,46 +2,64 @@
 
 const cdk = require('aws-cdk-lib');
 const { CenomiIntegrationCognitoStack } = require('../lib/cenomi-integration-cognito-stack');
-const { CenomiIntegrationCommonStack } = require('../lib/cenomi-integration-commmon-stack');
+const { CenomiIntegrationCommonStack } = require('../lib/cenomi-integration-common-stack');
 const { CenomiIntegrationDynamoDbStack } = require('../lib/cenomi-integration-dynamodb-stack');
-const { CenomiIntegrationBusinessStack } = require('../lib/cenomi-integration-buisness-stack');
+const { CenomiIntegrationBusinessStack } = require('../lib/cenomi-integration-business-stack');
 const { CenomiIntegrationLayersStack } = require('../lib/cenomi-integration-layers-stack');
 
 const app = new cdk.App();
 
 let config = {};
-config = app.node.tryGetContext('sit-config');
 
-new CenomiIntegrationCognitoStack(app, 'cenomi-integration-cognito-stack', {
-  resourcePrefix : config.project.projectName
+if (app.node.tryGetContext("deploy-environment") === "sit") {
+  config = app.node.tryGetContext("sit-config");
+} else if (app.node.tryGetContext("deploy-environment") === "uat") {
+  config = app.node.tryGetContext("uat-config");
+} else if (app.node.tryGetContext("deploy-environment") === "prod") {
+  config = app.node.tryGetContext("prod-config");
+}
+
+const projectResourcesPrefix =
+  config.project.projectName + "-" + config.project.projectEnvironment + "-";
+const StackPrefix =
+  config.project.projectName.toLowerCase() +
+  "-" +
+  config.project.projectEnvironment.toLowerCase() +
+  "-";
+
+
+new CenomiIntegrationCognitoStack(app, `${StackPrefix}cognito-stack`, {
+  resourcePrefix : projectResourcesPrefix
 });
 
-new CenomiIntegrationCommonStack(app,'cenomi-integration-common-stack',{
-  resourcePrefix : config.project.projectName,
+new CenomiIntegrationCommonStack(app,`${StackPrefix}common-stack`,{
+  resourcePrefix : projectResourcesPrefix,
   env: config.env,
   attributes: config.attributes,
   roles: config.roles,
   allowCrossOrigins: config.allowCrossOrigins,
 });
 
-new CenomiIntegrationDynamoDbStack(app, 'cenomi-integration-dynamodb-stack',{
-  resourcePrefix : config.project.projectName,
+new CenomiIntegrationDynamoDbStack(app, `${StackPrefix}dynamodb-stack`,{
+  resourcePrefix : projectResourcesPrefix,
   env: config.env,
+  dbTables: config.dynamoDBTables,
   attributes: config.attributes,
   roles: config.roles,
   allowCrossOrigins: config.allowCrossOrigins,
 });
-new CenomiIntegrationBusinessStack(app, 'cenomi-integration-buisness-stack',{
-  resourcePrefix : config.project.projectName,
+new CenomiIntegrationBusinessStack(app, `${StackPrefix}buisness-stack`,{
+  resourcePrefix : projectResourcesPrefix,
   env: config.env,
   attributes: config.attributes,
   roles: config.roles,
+  dbTables: config.dynamoDBTables,
   allowCrossOrigins: config.allowCrossOrigins,
   layersData:config.layers,
   runtime: cdk.aws_lambda.Runtime.NODEJS_18_X
 });
-new CenomiIntegrationLayersStack(app,'cenomi-integration-layers-stack',{
-  resourcePrefix : config.project.projectName,
+new CenomiIntegrationLayersStack(app,`${StackPrefix}layers-stack`,{
+  resourcePrefix : projectResourcesPrefix,
   layersData: config.layers,
   env: config.env
 });
