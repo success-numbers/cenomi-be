bec1ddbf372f8532d9fdcc3ba9c056ea88391d9c
shivanshu.s-at-942428920331 <shivanshu.sanjeev@clappia.com>
2023-12-11 14:10:06 +0530

    fileTransfer APIs written

--- fileTransfers/putFileTransfers/putFileTransfersLambda.js
+++ fileTransfers/putFileTransfers/putFileTransfersLambda.js
@@ -1,36 +1,91 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+// Import utility functions for payload validation
+const { validateALLOC, validateGRN, validateGSD, validateBRI } = require('./utilityFunctions');
+const { processALLOC, processGRN, processGSD, processBRI } = require('./processingFunctions');
+
 exports.handler = async (event) => {
     try {
-        const params = {
-            TableName: process.env.tableName,
-            IndexName: 'locationIndex',
-            ProjectionExpression: 'destIdGSK1', 
-        };
-
-        const result = await dynamoDb.scan(params).promise();
-        
-       
-        const destLocIds = Array.from(new Set(result.Items.map(item => {
-            const splitValues = item.destIdGSK1.split('#');
-            return splitValues[1]; // Extracting the destId value
-        })));
-
-        const response = {
-            destLocIds: destLocIds,
-            count: destLocIds.length
-        };
-
-        const res = {
-            statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify(response),
-        };
-        return res;
+        // const fileType = event.queryStringParameters.fileType;
+        const payload = JSON.parse(event.body);
+        const tableName = process.env.tableName;
+        let validationResponse;
+        switch (payload.fileType) {
+            case 'ALLOC':
+                validationResponse = validateALLOC(payload);
+                if (validationResponse.isValid) {
+                    const processingResponse = await processALLOC(payload,tableName);
+                    if (processingResponse.success) {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify({ message: 'Payload inserted successfully' }),
+                        };
+                    } else {
+                        return {
+                            statusCode: 500,
+                            body: JSON.stringify({ message: processingResponse.errorMessage }),
+                        };
+                    }
+                }
+                break;
+            case 'GRN':
+                validationResponse = validateGRN(payload);
+                if (validationResponse.isValid) {
+                    const processingResponse = await processGRN(payload,tableName);
+                    if (processingResponse.success) {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify({ message: 'Payload inserted successfully' }),
+                        };
+                    } else {
+                        return {
+                            statusCode: 500,
+                            body: JSON.stringify({ message: processingResponse.errorMessage }),
+                        };
+                    }
+                }
+                break;
+            case 'GSD':
+                validationResponse = validateGSD(payload);
+                if (validationResponse.isValid) {
+                    const processingResponse = await processGSD(payload,tableName);
+                    if (processingResponse.success) {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify({ message: 'Payload inserted successfully' }),
+                        };
+                    } else {
+                        return {
+                            statusCode: 500,
+                            body: JSON.stringify({ message: processingResponse.errorMessage }),
+                        };
+                    }
+                }
+                break;
+            case 'BRI':
+                validationResponse = validateBRI(payload);
+                if (validationResponse.isValid) {
+                    const processingResponse = await processBRI(payload,tableName);
+                    if (processingResponse.success) {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify({ message: 'Payload inserted successfully' }),
+                        };
+                    } else {
+                        return {
+                            statusCode: 500,
+                            body: JSON.stringify({ message: processingResponse.errorMessage }),
+                        };
+                    }
+                }
+                break;
+            default:
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: 'Invalid fileType' }),
+                };
+        }
     } catch (e) {
         console.error('Error fetching destLocIds:', e.message);
         const res = {
@@ -39,8 +94,24 @@ exports.handler = async (event) => {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
             },
-            body: JSON.stringify({ "message": "Internal server error" }),
+            body: JSON.stringify({ message: 'Internal server error' }),
         };
         return res;
     }
 };
+
+
+// {
+//     "fileName": "RTUSKKKFHHJX",
+//     "fileType": "ALLOC",
+//     "userId": "utsav",
+//     "createdAt": 1231937193719,
+//     "rows": [
+//         {
+//             "storeCode": "123",
+//             "barcode": "534555",           
+//             "quantity": 100,
+//             "brand": "Brand4"
+//         }
+//     ]
+// }
---
+++ fileTransfers/putFileTransfers/utilityFunctions.js
@@ -0,0 +1,102 @@
+function validateALLOC(payload) {
+    const requiredFields = ['storeCode', 'barcode', 'quantity', 'brand'];
+    const payloadRows = payload.rows;
+
+    for (const row of payloadRows) {
+        const payloadFields = Object.keys(row);
+
+        for (const field of requiredFields) {
+            if (!payloadFields.includes(field)) {
+                return {
+                    isValid: false,
+                    errorMessage: `Field '${field}' is missing in one of the rows for 'ALLOC'`,
+                };
+            }
+        }
+
+        if (payloadFields.length !== requiredFields.length) {
+            return {
+                isValid: false,
+                errorMessage: 'Extra or invalid fields found in one of the rows for \'ALLOC\'',
+            };
+        }
+    }
+
+    return { isValid: true };
+}
+
+function validateGRN(payload) {
+    const requiredFields = ['barcode'];
+    const payloadRows = payload.rows;
+
+    for (const row of payloadRows) {
+        const payloadFields = Object.keys(row);
+
+        for (const field of requiredFields) {
+            if (!payloadFields.includes(field)) {
+                return {
+                    isValid: false,
+                    errorMessage: `Field '${field}' is missing in one of the rows for 'GRN'`,
+                };
+            }
+        }
+
+        if (payloadFields.length !== requiredFields.length) {
+            return {
+                isValid: false,
+                errorMessage: 'Extra or invalid fields found in one of the rows for \'GRN\'',
+            };
+        }
+    }
+
+    return { isValid: true };
+}
+
+function validateGSD(payload) {
+    const requiredFields = ['storeCode', 'inputBoxId'];
+    const payloadRows = payload.rows;
+
+    for (const row of payloadRows) {
+        const payloadFields = Object.keys(row);
+
+        for (const field of requiredFields) {
+            if (!payloadFields.includes(field)) {
+                return {
+                    isValid: false,
+                    errorMessage: `Field '${field}' is missing in one of the rows for 'GSD'`,
+                };
+            }
+        }
+
+        if (payloadFields.length !== requiredFields.length) {
+            return {
+                isValid: false,
+                errorMessage: 'Extra or invalid fields found in one of the rows for \'GSD\'',
+            };
+        }
+    }
+
+    return { isValid: true };
+}
+
+function validateBRI(payload) {
+    const requiredFields = ['barcode'];
+    const payloadRows = payload.rows;
+
+    for (const row of payloadRows) {
+        const payloadFields = Object.keys(row);
+
+        for (const field of requiredFields) {
+            if (!payloadFields.includes(field)) {
+                return {
+                    isValid: false,
+                    errorMessage: `Field '${field}' is missing in one of the rows for 'BRI'`,
+                };
+            }
+        }
+    }
+
+    return { isValid: true };
+}
+
+module.exports = { validateALLOC, validateGRN, validateGSD, validateBRI };
