2430b80362a09d87124ebd528b20b152da318904
Balaji Krishnan <balaji@snumbers.com>
2023-12-14 10:36:14 +0530

    Fixes

--- fileTransfers/getAllFileTransferTypes/getAllFileTransferTypes.js
+++ fileTransfers/getAllFileTransferTypes/getAllFileTransferTypes.js
@@ -4,6 +4,36 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
     try {
         console.log("Get all file trn types STATS LAMBDA", JSON.stringify(event));
+        const mockResponse = [
+            {
+                "id": "ALLOC",
+                "headers": [
+                    "STORE CODE",
+                    "BARCODE",
+                    "QTY",
+                    "BRAND"
+                ]
+            },
+            {
+                "id": "GRN",
+                "headers": [
+                    "BARCODE"
+                ]
+            },
+            {
+                "id": "DSD",
+                "headers": [
+                    "STORE CODE",
+                    "INPUT BOX ID"
+                ]
+            },
+                {
+                "id": "BRI",
+                "headers": [
+                    "BARCODE"
+                ]
+            }
+        ]      
         const res = {
             statusCode: 200,
             headers: {
