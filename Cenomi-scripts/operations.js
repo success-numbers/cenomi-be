3f190ddeb6d04c03fb16375df4c72b5620f8e446
KamenRider90 <utsav.manjhi@snumbers.com>
2024-02-28 16:10:31 +0530

    changes in script

--- Cenomi-scripts/operations.js
+++ Cenomi-scripts/operations.js
@@ -7,5 +7,68 @@ module.exports = [
     "entityType": "OPERATION",
     "operationDesc": "USER MANAGEMENT",
     "operationId": "Operation1"
-    }
+    },
+    {
+        "PK": "1",
+        "SK": "1",
+        "active": true,
+        "createDate": 1704869794,
+        "entityType": "OPERATION",
+        "operationDesc": "INDITEX",
+        "operationId": "Operation2"
+    },
+    {
+        "PK": "2",
+        "SK": "2",
+        "active": true,
+        "createDate": 1704869801,
+        "entityType": "OPERATION",
+        "operationDesc": "ALLOCATION",
+        "operationId": "Operation3"
+       },
+       {
+        "PK": "3",
+        "SK": "3",
+        "active": true,
+        "createDate": 1704869809,
+        "entityType": "OPERATION",
+        "operationDesc": "DSD",
+        "operationId": "Operation4"
+       },
+       {
+        "PK": "4",
+        "SK": "4",
+        "active": true,
+        "createDate": 1704869809,
+        "entityType": "OPERATION",
+        "operationDesc": "GRN",
+        "operationId": "Operation5"
+       },
+       {
+        "PK": "5",
+        "SK": "5",
+        "active": true,
+        "createDate": 1704869809,
+        "entityType": "OPERATION",
+        "operationDesc": "INDITEX - ADMIN",
+        "operationId": "Operation6"
+       },
+       {
+        "PK": "6",
+        "SK": "6",
+        "active": true,
+        "createDate": 1704869809,
+        "entityType": "OPERATION",
+        "operationDesc": "FILE TRANSFERS",
+        "operationId": "Operation7"
+       },
+       {
+        "PK": "7",
+        "SK": "7",
+        "active": true,
+        "createDate": 1704869809,
+        "entityType": "OPERATION",
+        "operationDesc": "DASHBOARD",
+        "operationId": "Operation8"
+       }
 ]
